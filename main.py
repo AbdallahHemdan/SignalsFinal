c70efd64d17167f0daa8877e8fc7e376b13439e1
Mahboub99 <ahmed.mahboub99@gmail.com>
2020-05-15 13:34:51 +0200

    add setps[1:4] in decoder
    1 load the out-image
    2 display the compressed
          image
    3 Form a matrix for the
         deCompressed image with the
         original size
    4 Get each block of to be
         decompressed.

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-from scipy.fft import dct
+from scipy.fft import dct ,idct
 import numpy as np
 import cv2
 
 
 # Steps to follow
 
-# 1. Read the image file ‘image1.bmp’. => Done
-# 2. Extract and display each of its three color components. => Done
-# 3. Convert range of each component to [-128, 127] => Done
-# 4. Form a matrix for the outImage with the new size => Done
-# 5. Process each color component in blocks of 8×8 pixels. => Done
-# 6. Obtain 2D DCT of each block. => Done
-# 7. Retain only the top left square of the 2D DCT coefficients of size 𝑚 × 𝑚, The rest of coefficients are ignored. => Done
-# 8. Compare the size of the original and compressed images. => Done
-# 9. Decompress the image by applying inverse 2D DCT to each block. Display the image.
+# 1. Encoder
+#   1.1 Read the image file ‘image1.bmp’. => Done
+#   1.2 Extract and display each of its three color components. => Done
+#   1.3 Convert range of each component to [-128, 127] => Done
+#   1.4 Form a matrix for the outImage with the new size => Done
+#   1.5 Process each color component in blocks of 8×8 pixels. => Done
+#   1.6 Obtain 2D DCT of each block. => Done
+#   1.7 Retain only the top left square of the 2D DCT coefficients of size 𝑚 × 𝑚, The rest of coefficients are ignored. => Done
+#   1.8 Compare the size of the original and compressed images. => Done
+
+# 2. Decoder
+#   2.1 load the out-image=>Done
+#   2.2 display the compressed image=>Done
+#   2.3 Form a matrix for the deCompressed image with the original size => Done
+#   2.4 Get each block of to be decompressed.
+#   2.5 apply inverse dct on each block
+#   2.6 re-range the out image by adding 128 ranges from [0 : 255]
+#   2.7 display the decompressed image and Compare them
+#   2.8 quality of the decompressed image is measured using the Peak Signal-to-Noise Ratio PSNR)
+#   2.9 technical report (advantages of using DCT instead of DFT)
 
 
-# Step 3
+# Step 1.3
 def reRange(inputImage):
     print("inputImage before", inputImage)
     inputImage = inputImage.astype('int')
@@ -24,34 +35,7 @@ def reRange(inputImage):
     print("inputImage after", inputImage)
     return inputImage
 
-
-def imageCompression(inputImage, m, row, col):
-    # Step 4
-    outImage = np.zeros(
-        (int((row / 8) * m), int((col / 8) * m), 3), dtype=np.float16)
-
-    blockRow = int(row / 8)
-    blockCol = int(col / 8)
-    blockComponents = 3
-    noIterations = 0
-
-    # Step 5
-    for x in range(0, blockRow):
-        for y in range(0, blockCol):
-            for z in range(0, blockComponents):
-                noIterations += 1
-                currentBlock = inputImage[x *
-                                          8: x * 8 + 8, y * 8: y * 8 + 8, z]
-                # Step 6, 7
-                blockDCT = dct(dct(currentBlock.T, norm='ortho').T,
-                               norm='ortho')[0:m, 0:m]
-                outImage[x * m: x * m + m, y * m: y * m + m, z] = blockDCT
-    print("no Iterations", noIterations)
-    print("outImage", outImage)
-    return outImage
-
-
-# Step 2
+# Step 1.2
 def getComponent(inputImage, no):
     # 1. no = 0 => red
     # 2. no = 1 => green
@@ -63,7 +47,52 @@ def getComponent(inputImage, no):
     return cpy
 
 
-# Step 1
+
+def imageCompression(inputImage, m, row, col):
+    # Step 1.4
+    outImage = np.zeros(
+        (int((row / 8) * m), int((col / 8) * m), 3), dtype=np.float16)
+
+    blockRow = int(row / 8)
+    blockCol = int(col / 8)
+    blockComponents = 3
+    noIterations = 0
+
+    # Step 1.5
+    for x in range(0, blockRow):
+        for y in range(0, blockCol):
+            for z in range(0, blockComponents):
+                noIterations += 1
+                currentBlock = inputImage[x *
+                                          8: x * 8 + 8, y * 8: y * 8 + 8, z]
+                # Step 1.6, 1.7
+                blockDCT = dct(dct(currentBlock.T, norm='ortho').T, norm='ortho')[0:m, 0:m]
+                outImage[x * m: x * m + m, y * m: y * m + m, z] = blockDCT
+    print("no Iterations", noIterations)
+    print("outImage", outImage)
+    return outImage
+
+
+
+def imageDeCompression(toBeCompressedImage ,m , row ,col):
+    # Step 2.3
+    deCompressedImage = np.zeros(int((row/m)*8, int((col/m)*8)), 3, dtype=np.float16)
+
+    blockRow = int(row / m)
+    blockCol = int(col / m)
+    blockComponents = 3
+    noIterations = 0
+
+    # Step 2.4
+    for x in range(0, blockRow):
+        for y in range(0, blockCol):
+            for z in range(0, blockComponents):
+                noIterations += 1
+                currentBlock = toBeCompressedImage[x * m: x * m + m, y * m: y * m + m, z]
+                deCompressedBlock = np.zeros(8, 8, 3)
+                deCompressedBlock[0: m, 0: m, 0]
+
+# Step 1.1
 inputImage = cv2.imread('./image1.bmp')
 np.save("inputImage", inputImage)
 
@@ -76,7 +105,7 @@ cv2.waitKey(0)
 cv2.destroyAllWindows()
 
 
-# Step 2
+# Step 1.2
 # Get Red Component
 blueComponent = getComponent(inputImage, 2)
 cv2.imshow("Red Component", blueComponent)
@@ -89,16 +118,26 @@ cv2.imshow("Green Component", greenComponent)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
+
+
 # Get Blue Component
 redComponent = getComponent(inputImage, 0)
 cv2.imshow("Blue Component", redComponent)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
-# Step 3
+# Step 1.3
 inputImage = reRange(inputImage)
 
-# Step 8
+# Step 1.8
 outImage = imageCompression(inputImage, m, row, col)
 print("Output Image", outImage)
 np.save("outImage", outImage)
+
+# Step 2.1
+toBeCompressedImage = np.load("outImage.npy")
+
+# Step 2.2
+print("decompressed", toBeCompressedImage)
+deRow = toBeCompressedImage.shape[0]
+deCol = toBeCompressedImage.shape[1]
