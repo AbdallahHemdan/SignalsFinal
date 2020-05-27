d721fa8de182d0c6dc8c1652c493fd657303eaac
Ahmed Ashraf <ahmed.ahmed991@eng-st.cu.edu.eg>
2020-05-28 01:36:28 +0200

    Update Task2.m

--- Task 2/Task2.m
+++ Task 2/Task2.m
@@ -1,4 +1,7 @@
 clear all;
+% Author: Ahmed Ashraf Hamdy
+
+% Read two audio files
 % %audio1 duration is less than audio2
 [x1, fs1] = audioread("audio1.wav");
 number_of_samples1 = length(x1);
@@ -10,19 +13,21 @@ duration_in_seconds1 = floor(number_of_samples1 / fs1);
 number_of_samples2 = length(x2);
 duration_in_seconds2 = floor(number_of_samples2 / fs2);
 
+% Hide audio1 in audio2
 [~,peaks1] = findpeaks(x1);
 N1 = mean(diff(peaks1));
 [~,peaks2] = findpeaks(x2);
 N2 = mean(diff(peaks2));
 omega = 2.4;  %2pi/period
-A = 0.1;
+A = 0.05;
 
 X = x2;
 for n = 1 : number_of_samples1
     X(n) = x2(n)+A*x1(n)*cos(omega*n);
 end
 
-figure; plot(log10(abs(fft(X)))); savefig('magnitudeSpectrum.jpeg')
+% Plot the magnitude spectrum of X 
+figure; plot(log10(abs(fft(X))));
 sound(X, fs2);
 pause(duration_in_seconds2);
 audiowrite("newAudio2.wav",X,fs2);
@@ -32,7 +37,7 @@ audiowrite("newAudio2.wav",X,fs2);
 % figure('Name', 'the magnitude spectrum for audio1'); plot((abs(fft(x1))));
 % % figure('Name', 'the magnitude spectrum for audio2'); plot(log10(abs(fft(x2))));
 % 
-% 
+% restore x1[n]
 Y = linspace(0, duration_in_seconds1, number_of_samples1);
 for n = 1 : min(length(x1), length(x2))
     Y(n) = X(n)*cos(omega*n);
@@ -46,10 +51,10 @@ for k = floor(length(Yfft)/range) : (range)*floor(length(Yfft)/range)
 end
 % delete the effect of the attenuation factor by dividing by it and amplify the signal.
 for n = 1 : length(Yfft)
-    Yfft(n) = 2 * (Yfft(n) / A);
+    Yfft(n) = 4 * (Yfft(n) / A);
 end
 % % inverse fourier transform
-Yifft = abs(ifft(Yfft));
+Yifft = real(ifft(Yfft));
 sound(Yifft, fs1);
 pause(duration_in_seconds1);
-audiowrite("newAudio1.wav",Y,fs1);
+audiowrite("newAudio1.wav",Yifft,fs1);
